886987d635803422ad377218029493c5cc457b6f
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 04:16:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index(1) do |element, index|
+  array.each_with_index(1) |element, index|
     puts "#{index}. #{element}"
   end
   # Use `each_with_index` to enumerate over the provided array
