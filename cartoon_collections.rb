c7ffa70c7fee58d4791f9e0724453a9720da21bd
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 04:24:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,14 @@ def greet_characters(array)
   # Print a custom greeting for each element
 end
 
-def list_dwarves(array)
-  array.each_with_index(1) do |element, index| 
-  puts "#{index}. #{element}"
-end
-end
+# def list_dwarves(array)
+#   array.each_with_index(1) do |element, index| 
+#   puts "#{index}. #{element}"
+# end
+# end
 
+def list_dwarves(array)
 array.each_with_index { |item, index|
   puts "#{index+1}. #{item}"
 }
+end
